9b95e423b0f23259ae6c8dbca5f97e2144016298
Angel98Canvia <117381798+Angel98Canvia@users.noreply.github.com>
2022-12-05 22:31:14 -0500

    first commit

---
+++ fraud/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from fraud.users.models import User
+from fraud.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
